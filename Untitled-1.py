b677d2a948b29adfab907f53c8aaa1279c61a014
mahmoud91-91 <ie.mahmoudijavad@gmail.com>
2024-07-09 10:11:04 +0330

    Update and add for loop to Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,11 @@
 print("new project")
 print("be aware")
 print("i am mad at python")
+f=[10,12,14,15,8,7,22]
+g=[33,9,31,4,16,6,7,23]
+for i in f:
+  for j in g:
+    if i>j:
+      print("///////////////////")
+    else:
+      print("********************")
